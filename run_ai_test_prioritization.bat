2d5acf5751720bb61c49098e853ed9eb33b16e3f
SAURABH SINGH <72145932+saurabhkingp@users.noreply.github.com>
2025-07-06 22:26:59 +0530

    fixed batch file

--- run_ai_test_prioritization.bat
+++ run_ai_test_prioritization.bat
@@ -1,15 +1,12 @@
 @echo off
 REM Run the AI Test Prioritization main script.
 
-IF EXIST .venv\Scripts\activate.bat (
-    call .venv\Scripts\activate.bat
-)
+CALL c:/Users/saura/OneDrive/Desktop/open_source_venv/Scripts/activate.bat
 
 echo([32m==== Running AI Test Prioritization ====
 
 python -m src.main
 
-REM Prints end message based on exit code
 if %ERRORLEVEL% EQU 0 (
     echo([32m==== AI Test Prioritization Completed Successfully ==== [0m
     exit /b 0
